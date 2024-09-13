5a2c040732bebc8ae0888f24e6ce0cb2608f4c29
Martin Pravda <martin.pravda@gmail.com>
2024-09-13 17:47:08 +0200

    components

---
+++ components.js
@@ -0,0 +1,12 @@
+// components.js
+// Martin Pravda
+
+/*jslint browser */
+
+import background_switcher from "./background_switcher.js";
+import map from "./map.js";
+
+export default Object.freeze({
+    background_switcher,
+    map
+});
