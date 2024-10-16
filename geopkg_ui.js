92534d73fab21b5e0673825ab8fb9b4428fe630c
Martin Pravda <martin.pravda@gmail.com>
2024-10-16 17:22:24 +0200

    geopkg_ui

--- geopkg_ui.js
+++ geopkg_ui.js
@@ -1,5 +1,6 @@
 // geopkg_ui.js
 // Martin Pravda
+// UI component for manipulating with geopackage format
 
 /* jslint browser*/
 
@@ -7,11 +8,11 @@
 import make_ui from "./ui.js";
 import geopkg_conn_ui from "./geopkg_conn_ui.js";
 import geopkg_table_ui from "./geopkg_table_ui.js";
-import geopkg_controls_ui  from "./geopkg_controls_ui.js";
+import geopkg_controls_ui from "./geopkg_controls_ui.js";
 
 const geopkg_ui = make_ui("geopkg-ui", function (element, {
     db,
-    map,
+    on_add,
     on_close
 }) {
     let connections;
@@ -19,35 +20,40 @@ const geopkg_ui = make_ui("geopkg-ui", function (element, {
     let controls;
     const shadow = element.attachShadow({mode: "closed"});
 
+    function on_connect({geopackage}) {
+        if (table) {
+            table.remove();
+        }
+        const [result] = geopackage.get_feature_tables();
+
+        table = geopkg_table_ui({
+            columns: result.columns,
+            on_row_click: function (row) {
+                const [table_name] = row;
+                controls.remove_listeners();
+                controls.attach_listeners({
+                    on_add: function () {
+                        const [feature] = geopackage.get_feature(
+                            table_name
+                        );
+
+                        on_add(feature);
+                    }
+                });
+            },
+            rows: result.values
+        });
+
+        shadow.append(table);
+    }
+
     connections = geopkg_conn_ui({
         db,
-        on_connect: function on_connect({geopackage}) {
-            if (table) {
-                table.remove();
-            }
-            const [result] = geopackage.get_feature_tables();
-
-            table = geopkg_table_ui({
-                columns: result.columns,
-                on_row_click: function (row) {
-                    const [table_name] = row;
-                    controls.remove_listeners();
-                    controls.attach_listeners({
-                        on_add: function () {
-                            const [feature] = geopackage.get_feature(
-                                table_name
-                            );
-                            console.log(feature);
-                        }
-                    });
-                },
-                rows: result.values
-            });
-
-            shadow.append(table);
-
-        },
-        on_remove: function () {}
+        on_connect,
+        on_remove: function () {
+            controls?.remove_listeners();
+            table?.remove();
+        }
     });
 
     controls = geopkg_controls_ui({
